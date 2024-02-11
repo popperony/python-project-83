93cbcb18323586e66e1a3426930eee711136f2dd
popperony <popperony@yandex.ru>
2024-02-11 22:27:16 +0300

    update flask and gitignore

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,11 @@
+import os
+import requests
+from dotenv import load_dotenv
 from flask import Flask
+
+load_dotenv()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def hello_world():
