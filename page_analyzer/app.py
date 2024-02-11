76855df727ce78987fca50f9566bbf513e8a1b88
popperony <popperony@yandex.ru>
2024-02-11 22:38:53 +0300

    add index

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,31 @@
 import os
 import requests
 from dotenv import load_dotenv
-from flask import Flask
-
+from flask import (
+    Flask,
+    render_template,
+    request,
+    flash,
+    get_flashed_messages,
+    url_for,
+    redirect
+)
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+# @app.get('/urls')
+# def get_urls():
+#     """
+#         Get all urls from DB
+#         Show all urls
+#     """
+#     urls = get_all_urls()
+#     return render_template(
+#         'urls.html',
+#         urls=urls
+#     )
